372e6df03f9d415a0d7f47e0ed0f736ad17b7a5c
tedyshu <gs1buritos7@hotmail.com>
2024-05-09 09:51:43 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function maxProduct(nums) {
-  if (nums.length === 0) return 0;
-  let maxSoFar = nums[0];
-  let minSoFar = nums[0];
-  let maxProduct = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    const temp = maxSoFar;
-    maxSoFar = Math.max(nums[i], nums[i] * maxSoFar, nums[i] * minSoFar);
-    minSoFar = Math.min(nums[i], nums[i] * temp, nums[i] * minSoFar);
-    maxProduct = Math.max(maxProduct, maxSoFar);
-  }
-  return maxProduct;
-}
+const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
