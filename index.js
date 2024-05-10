11d0f4c6d3f760ea1c19a2f980dbdb52eccf6eba
tedyshu <gs1buritos7@hotmail.com>
2024-05-10 21:45:46 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+};
