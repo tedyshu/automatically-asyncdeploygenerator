73af69e198944b023f00116a48fa7cb08b4c3f1d
tedyshu <gs1buritos7@hotmail.com>
2024-05-12 09:07:15 +0700

    start project

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-function mergeKLists(lists) {
-  if (!lists || !lists.length) return null;
-  return mergeLists(lists, 0, lists.length - 1);
-  function mergeLists(lists, start, end) {
-    if (start === end) return lists[start];
-    const mid = Math.floor((start + end) / 2);
-    const left = mergeLists(lists, start, mid);
-    const right = mergeLists(lists, mid + 1, end);
-    return mergeTwoLists(left, right);
-  }
-  function mergeTwoLists(l1, l2) {
-    if (!l1) return l2;
-    if (!l2) return l1;
-    if (l1.val < l2.val) {
-      l1.next = mergeTwoLists(l1.next, l2);
-      return l1;
-    } else {
-      l2.next = mergeTwoLists(l1, l2.next);
-      return l2;
+function numDecodings(s) {
+  const dp = new Array(s.length + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = s[0] === "0" ? 0 : 1;
+  for (let i = 2; i <= s.length; i++) {
+    const oneDigit = parseInt(s.substring(i - 1, i));
+    const twoDigits = parseInt(s.substring(i - 2, i));
+    if (oneDigit >= 1) {
+      dp[i] += dp[i - 1];
+    }
+    if (twoDigits >= 10 && twoDigits <= 26) {
+      dp[i] += dp[i - 2];
     }
   }
+  return dp[s.length];
 }
